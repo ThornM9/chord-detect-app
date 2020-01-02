01188ddf225ce6ad2023f2f54ad8d5715cd6e34f
ThornM9 <tm.jalal@gmail.com>
2020-01-03 08:54:01 +1000

    server on https in production env

--- server/server.js
+++ server/server.js
@@ -4,13 +4,14 @@ const path = require("path");
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const processwav = require("./process_wav");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const greenlock = require("greenlock-express");
 dotenv.config();
 const PORT = process.env.PORT || 5000
 
 const frontendDomain = (process.env.NODE_ENV === 'production')
     ? 'http://chorddetect.com'
-    : 'http://127.0.0.1:8080'
+    : 'http://127.0.0.1:8080';
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", frontendDomain); // update to match the domain you will make the request from
@@ -28,4 +29,17 @@ app.post("/upload_wav", upload.single("wav"), function(req, res) {
 
 app.use("/public", express.static("./public"));
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// run the server on https in production
+if (process.env.NODE_ENV === "production") {
+    // SSL cert 
+    https.createServer({
+        key: fs.readFileSync('/etc/letsencrypt/live/chorddetect.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/chorddetect.com/cert.pem')
+    }, app)
+    .listen(PORT, function () {
+        console.log(`Listening for HTTPS on ${ PORT }`)
+    })
+} else {
+    app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+}
